3296569c36471c85da57af8bcb8fb75ca6c7e084
Nathan Rocha <neroite71@hotmail.com>
2025-04-29 21:38:02 -0300

    Criando FrontEnd

---
+++ Lopobia/Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lopobia.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmarApagar()
+        {
+            return View();
+        }
+    }
+}
